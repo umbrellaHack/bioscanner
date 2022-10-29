be0a847dedcf1d136848274a339789170d5f7d19
vian21 <pigir101@uotawa.ca>
2022-10-29 01:55:24 -0400

    integrate navbar into about.html

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,14 @@
 function toggleDarkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
+
+    // switch navbar color
+    var element = document.getElementById("navbar");
+    element.classList.toggle("dark-mode");
+
+    // change the hamburger icon in navbar
+    element.classList.toggle("navbar-dark");
+
 }
 
 window.onload = (event) => {
